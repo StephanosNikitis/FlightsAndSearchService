a76b206430b5ec9107d70d495cd72b2ae7c188df
StephanosNikitis <theshaurya035@gmail.com>
2025-07-15 19:43:16 +0530

    Completed flight update api

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -24,10 +24,30 @@ class FlightService {
         }
     }
 
-    async  getFlightData(data) {
+    async  getAllFlightData(data) {
         try {
             const flights = await this.flightRepository.getAllFlights(data)
             return flights
+        } catch (error) {
+            console.log('Something went wrong at service layer');
+            throw error
+        }
+    }
+
+    async getFlight(flightId) {
+        try {
+            const flight = await this.flightRepository.getFlight(flightId)
+            return flight
+        } catch (error) {
+            console.log('Something went wrong at service layer');
+            throw error
+        }
+    }
+
+    async updateFlight(flightId, data) {
+        try {
+            const response = await this.flightRepository.updateFlight(flightId, data)
+            return response
         } catch (error) {
             console.log('Something went wrong at service layer');
             throw error
